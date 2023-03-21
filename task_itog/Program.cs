0b2a4bb8adafa02a266683c1852c238db8c2459c
avk1702 <avk1702@bk.ru>
2023-03-21 13:37:47 +0500

    Добавлено решение

---
+++ task_itog/Program.cs
@@ -0,0 +1,24 @@
+﻿void SecondArray(string[] array_first, string[] array_second)
+{
+    int count = 0;
+    for (int i = 0; i < array_first.Length; i++)
+    {
+    if(array_first[i].Length <= 3)
+        {
+        array_second[count] = array_first[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+string[] array_first = new string[6] {"Тобол", "145", "number", "45", "Тверь", "Ок"};
+string[] array_second = new string[array_first.Length];
+SecondArray(array_first, array_second);
+PrintArray(array_second);
